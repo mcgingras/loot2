0660d778e0899793676a99d2b446e5e9a9b1a974
Michael Gingras <mcg79@cornell.edu>
2023-09-18 15:03:04 -0600

    optimistic update for deploying TBA

--- src/components/DeployTBAButtonClient.tsx
+++ src/components/DeployTBAButtonClient.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState, useTransition } from "react";
 import {
   ACCOUNT_IMPLEMENTATION_CONTRACT_ADDRESS,
   CHARACTER_CONTRACT_ADDRESS,
@@ -14,12 +15,17 @@ import {
   useContractWrite,
   usePrepareContractWrite,
   useContractRead,
+  useWaitForTransaction,
 } from "wagmi";
+import { useRouter } from "next/navigation";
 import { encodeFunctionData } from "viem";
 import { QuestionMarkCircleIcon } from "@heroicons/react/24/solid";
 import Tooltip from "@/components/Tooltip";
 
 const DeployTBAButtonClient = ({ tokenId }: { tokenId: bigint }) => {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [pending, setPending] = useState<boolean>(false);
   const { data: tbaAddress } = useContractRead({
     chainId: 84531,
     address: REGISTRY_CONTRACT_ADDRESS,
@@ -36,7 +42,7 @@ const DeployTBAButtonClient = ({ tokenId }: { tokenId: bigint }) => {
 
   // This prepare call doesn't even matter -- we are just trying to call it to see if it fails
   // If it fails we know we need to deploy a TBA
-  const { error } = usePrepareContractWrite({
+  const { error, refetch } = usePrepareContractWrite({
     chainId: 84531,
     address: tbaAddress,
     abi: AccountABI,
@@ -68,7 +74,25 @@ const DeployTBAButtonClient = ({ tokenId }: { tokenId: bigint }) => {
     ],
   });
 
-  const { write: createTBA } = useContractWrite(createTBAConfig);
+  const { data, write: createTBA } = useContractWrite({
+    ...createTBAConfig,
+    onSuccess: (_data) => {
+      setPending(true);
+    },
+  });
+
+  useWaitForTransaction({
+    chainId: 84531,
+    hash: data?.hash,
+    onSuccess: (_data) => {
+      setPending(false);
+      startTransition(() => {
+        // Refresh the current route and fetch new data from the server without
+        // losing client-side browser or React state.
+        refetch();
+      });
+    },
+  });
 
   const deployedTBA = !error?.message.includes("returned no data");
 
@@ -78,14 +102,22 @@ const DeployTBAButtonClient = ({ tokenId }: { tokenId: bigint }) => {
 
   return (
     <button
-      className="flex flex-row space-x-1 items-center justify-center mx-auto bg-white w-full mt-4 py-2 rounded"
+      className={`flex flex-row space-x-1 items-center justify-center mx-auto bg-white w-full mt-4 py-2 rounded ${
+        pending && "animate-pulse"
+      }`}
       onClick={() => createTBA?.()}
     >
-      <span>Deploy TBA</span>
-      <Tooltip
-        button={<QuestionMarkCircleIcon className="h-4 w-4 text-black" />}
-        text="Before you can equip traits, you need to activate your character by deploying a token bound account (TBA). This is a one-time action."
-      />
+      {pending ? (
+        "Pending..."
+      ) : (
+        <>
+          <span>Deploy TBA</span>
+          <Tooltip
+            button={<QuestionMarkCircleIcon className="h-4 w-4 text-black" />}
+            text="Before you can equip traits, you need to activate your character by deploying a token bound account (TBA). This is a one-time action."
+          />
+        </>
+      )}
     </button>
   );
 };
