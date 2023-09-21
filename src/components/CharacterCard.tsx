4d131d6513743c1e9f0488a305abefb59b83f76c
Michael Gingras <mcg79@cornell.edu>
2023-09-21 13:52:48 -0600

    Attempts to debug RSC error

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -3,18 +3,27 @@ import { CharacterABI } from "@/abi/character";
 import { activeClient } from "@/lib/viem";
 
 const getTokenURI = async (tokenId: bigint) => {
-  const data = await activeClient.readContract({
-    address: CHARACTER_CONTRACT_ADDRESS,
-    abi: CharacterABI,
-    functionName: "tokenURI",
-    args: [tokenId],
-  });
+  try {
+    const data = await activeClient.readContract({
+      address: CHARACTER_CONTRACT_ADDRESS,
+      abi: CharacterABI,
+      functionName: "tokenURI",
+      args: [tokenId],
+    });
 
-  return data;
+    return data;
+  } catch (e) {
+    console.log(e);
+    return "ERROR";
+  }
 };
 
 const CharacterCard = async ({ tokenId }: { tokenId: bigint }) => {
   const tokenURI = await getTokenURI(tokenId);
+
+  if (tokenURI === "ERROR") {
+    return <img className="border border-white/20" alt="Token details." />;
+  }
 
   // TODO:
   // return empty state from contract with same format as regular non empty state so we don't have to do this jank parse
